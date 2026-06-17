bfb1c76835f7717671d80dc6d3ad89b769f481f2
CabreraRFacundo <facundocabrerarodriguez@gmail.com>
2026-06-17 00:51:47 -0300

    wip

--- album_romantico/src/data/photos.js
+++ album_romantico/src/data/photos.js
@@ -3,24 +3,14 @@ function shuffle(array) {
 
     for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
 
     return arr;
 }
 
-export const photos = [
-    {
-        image: "/fotitos/a1.jpg",
-    },
-
-    ...shuffle(
-        Array.from(
-            { length: 16 },
-            (_, i) => ({
-                image: `/fotitos/a${i + 2}.jpg`,
-            })
-        )
-    ),
-];
+export const photos = shuffle(
+    Array.from({ length: 17 }, (_, i) => ({
+        image: `${import.meta.env.BASE_URL}fotitos/a${i + 1}.jpg`,
+    }))
+);
